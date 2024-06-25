804d740c15d3c2a54f2b9aea2673f11fcace40f4
Ljojua15 <jojualasha15@gmail.com>
2024-06-25 17:15:00 +0400

    'rx-js'

--- src/app/rx-js/rx-js.component.ts
+++ src/app/rx-js/rx-js.component.ts
@@ -1,6 +1,7 @@
+import { CurrencyPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
-import { fromEvent, map } from 'rxjs';
+import { fromEvent, map, scan, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-rx-js',
@@ -12,18 +13,34 @@ import { fromEvent, map } from 'rxjs';
 export class RxJsComponent implements OnInit {
   myTest$ = fromEvent(document, 'click');
   private http = inject(HttpClient);
+  x!: any;
+  capital: string = 'moscow';
 
   ngOnInit(): void {
-    this.myTest$.subscribe((x: any) => {
-      console.log(x.altitudeAngle);
+    this.myTest$.subscribe(() => {
+      console.log('x');
     });
+
     this.http
-      .get('https://restcountries.com/v3.1/capital/tb')
+      .get(`https://restcountries.com/v3.1/capital/${this.capital}`)
       .pipe(
         map((res: any) => {
-          console.log(res[0].borders);
+          console.log(res[0], 'sadas');
+
+          return {
+            countryName: res[0].altSpellings[1],
+            flag: res[0].flags.png,
+            Currency: (Object.values(res[0].currencies)[0] as any).name,
+            symbol: (Object.values(res[0].currencies)[0] as any).symbol,
+
+            border: res[0].borders,
+            continents: res[0].continents,
+          };
         })
       )
-      .subscribe((response: any) => {});
+      .subscribe((response: any) => {
+        this.x = response;
+        console.log(this.x);
+      });
   }
 }
