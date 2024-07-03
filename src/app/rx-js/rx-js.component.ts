349957b7e54b1fe3a0bf0c3683731f504d939860
Ljojua15 <jojualasha15@gmail.com>
2024-07-03 18:09:11 +0400

    add_mapUrl

--- src/app/rx-js/rx-js.component.ts
+++ src/app/rx-js/rx-js.component.ts
@@ -1,21 +1,28 @@
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, SecurityContext, inject } from '@angular/core';
+import {
+  DomSanitizer,
+  SafeResourceUrl,
+  SafeUrl,
+} from '@angular/platform-browser';
 import { fromEvent, map, scan, throttleTime } from 'rxjs';
+import { SafeUrlPipe } from '../../pipe/safe-url.pipe';
 
 @Component({
   selector: 'app-rx-js',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './rx-js.component.html',
   styleUrl: './rx-js.component.scss',
+  imports: [CommonModule, SafeUrlPipe],
 })
 export class RxJsComponent implements OnInit {
   myTest$ = fromEvent(document, 'click');
   private http = inject(HttpClient);
+  private sanitaizer = inject(DomSanitizer);
   x!: any;
   capital: string = 'tbilisi';
-
+  mapUrl!: any;
   ngOnInit(): void {
     this.myTest$.subscribe(() => {
       console.log('x');
@@ -27,24 +34,47 @@ export class RxJsComponent implements OnInit {
         map((res: any) => {
           console.log(res[0], 'sadas');
 
+          const all = res[0];
+
           return {
-            countryName: res[0].name.common,
-            flag: res[0].flags.png,
-            Currency: (Object.values(res[0].currencies)[0] as any).name,
-            symbol: (Object.values(res[0].currencies)[0] as any).symbol,
-            border: res[0].borders,
-            continents: res[0].continents,
-            languages: Object.values(res[0].languages)[0] as any,
-            region: res[0].region,
-            weekDayes: res[0].startOfWeek,
-            map: res[0].maps.googleMaps,
-            population: res[0].population,
+            countryName: all.name.common,
+            flag: all.flags.png,
+            Currency: (Object.values(all.currencies)[0] as any).name,
+            symbol: (Object.values(all.currencies)[0] as any).symbol,
+            border: all.borders,
+            continents: all.continents,
+            languages: Object.values(all.languages)[0] as any,
+            region: all.region,
+            weekDayes: all.startOfWeek,
+            map: this.transformToEmbedUrl(all.maps.googleMaps),
+            population: all.population,
           };
         })
       )
       .subscribe((response: any) => {
         this.x = response;
-        console.log(this.x);
+
+        this.mapUrl = response.map;
+        console.log(this.mapUrl.changingThisBreaksApplicationSecurity, '123');
       });
+  }
+
+  transformToEmbedUrl(y: string): string {
+    // Check if the URL contains '/maps/'
+    const mapsIndex = y.indexOf('/maps/');
+    console.log(mapsIndex);
+
+    if (mapsIndex !== -1) {
+      // Split the URL into two parts
+      const baseUrl = y.substring(0, mapsIndex + 6);
+      const placeId = y.substring(mapsIndex + 6);
+
+      // Construct the embed URL
+      const embedUrl = `${baseUrl}embed/v1/place/${placeId}`;
+      return embedUrl;
+    } else {
+      // If '/maps/' segment is not found, return the original URL
+      return y;
+    }
   }
 }
